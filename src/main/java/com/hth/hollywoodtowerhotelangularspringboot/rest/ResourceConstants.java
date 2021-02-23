69f98ab6b5f49ccbc786448b4823dcae27f9cda5
Patrick Cavanagh <pdcavanagh@yahoo.com>
2021-02-23 01:18:29 -0500

    initial commit, REST API, angular starter

---
+++ src/main/java/com/hth/hollywoodtowerhotelangularspringboot/rest/ResourceConstants.java
@@ -0,0 +1,5 @@
+package com.hth.hollywoodtowerhotelangularspringboot.rest;
+
+public class ResourceConstants {
+	public static final String ROOM_RESERVATION_V1="/room/reservation/v1";
+}
---
+++ src/test/java/com/hth/hollywoodtowerhotelangularspringboot/HollywoodTowerHotelAngularSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hth.hollywoodtowerhotelangularspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HollywoodTowerHotelAngularSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

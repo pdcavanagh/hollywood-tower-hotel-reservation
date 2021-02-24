a3ed38f228d5028c420b8f2a5137d51ce6d74954
Patrick Cavanagh <pdcavanagh@yahoo.com>
2021-02-24 01:54:17 -0500

    Built out Angular checkin/checkout form, skeleton markup

--- src/main/webapp/angular2/src/app/app.component.ts
+++ src/main/webapp/angular2/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular2';
+
+export class AppComponent implements OnInit
+{
+	public submitted : boolean = false;
+  	roomsearch : FormGroup;
+
+    constructor(fb: FormBuilder) {
+        this.roomsearch = fb.group({
+            title: fb.control('initial value', Validators.required)
+        });
+    }
+
+	ngOnInit() {
+		this.roomsearch = new FormGroup({
+			checkin: new FormControl(''),
+			checkout: new FormControl('')
+		});
+	}
+	
+	onSubmit({value,valid}: {value:Roomsearch, valid:boolean} ) {
+		console.log(value);
+	}
+
+}
+
+export interface Roomsearch {
+	checkin:string;
+	checkout:string;
 }

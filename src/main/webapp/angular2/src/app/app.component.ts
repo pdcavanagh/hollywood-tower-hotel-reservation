d001a70e1390c0b4a2eedf746491bbaeb745b827
Patrick Cavanagh <pdcavanagh@yahoo.com>
2021-02-25 01:59:59 -0500

    available room table using NgFor structural directive, moved css

--- src/main/webapp/angular2/src/app/app.component.ts
+++ src/main/webapp/angular2/src/app/app.component.ts
@@ -11,11 +11,14 @@ export class AppComponent implements OnInit
 {
 	public submitted : boolean = false;
   	roomsearch : FormGroup;
+	rooms : Room[];
 
     constructor(fb: FormBuilder) {
         this.roomsearch = fb.group({
             title: fb.control('initial value', Validators.required)
         });
+
+		this.rooms = [];
     }
 
 	ngOnInit() {
@@ -23,10 +26,16 @@ export class AppComponent implements OnInit
 			checkin: new FormControl(''),
 			checkout: new FormControl('')
 		});
+		
+		this.rooms = ROOMS;
 	}
 	
 	onSubmit({value,valid}: {value:Roomsearch, valid:boolean} ) {
 		console.log(value);
+	}
+	
+	reserveRoom(value:string) {
+		console.log("Room reservation id: " + value);
 	}
 
 }
@@ -35,3 +44,31 @@ export interface Roomsearch {
 	checkin:string;
 	checkout:string;
 }
+
+export interface Room {
+	id:string;
+	roomNumber:string;
+	price:string;
+	links:string;
+}
+
+let ROOMS:Room[] = [
+	{
+		id: "34575672",
+		roomNumber: "1301",
+		price: "275",
+		links: ""
+	},
+	{
+		id: "92016234",
+		roomNumber: "1302",
+		price: "295",
+		links: ""
+	},
+	{
+		id: "10239751",
+		roomNumber: "1303",
+		price: "305",
+		links: ""
+	}
+]
